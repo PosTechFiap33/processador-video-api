58cdb1da86213aff7b225e5ba58787f9d8c25690
flaviorobertoteixeira <flavio.r.teixeira@outlook.com>
2025-01-22 21:17:27 -0300

    chore: ajustando porta de direcionamento do security group

--- src/infra/sg.tf
+++ src/infra/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "sg" {
   ingress {
     description = "HTTP"
     from_port   = 80
-    to_port     = 80
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
